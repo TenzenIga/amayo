f2c197fce7271bb2a4e71419756faf97f3d8504d
TenzenIga <arhan-93@list.ru>
2025-11-08 07:33:54 +0300

    FR-19: убрал кнопку в мобильной версии, перенес функционал в навбар. Добавил иконку для уведомлений

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -2,38 +2,34 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { AuthService } from '@core/services/auth.service';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { DropdownComponent } from './dropdown/dropdown.component';
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faPlus, faBell } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SidebarService } from '@core/services/sidebar.service';
 @Component({
-    selector: 'app-navbar',
-    templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    standalone: true,
-    imports: [
-        RouterLink,
-        DropdownComponent,
-        RouterLinkActive, 
-        FontAwesomeModule
-        
-    ],
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [RouterLink, DropdownComponent, RouterLinkActive, FontAwesomeModule]
 })
 export class NavbarComponent {
-  private authService:AuthService = inject(AuthService);
-  private sidebarService:SidebarService = inject(SidebarService);
+  private authService: AuthService = inject(AuthService);
+  private sidebarService: SidebarService = inject(SidebarService);
   protected readonly router: Router = inject(Router);
   public faBars = faBars;
+  public faPlus = faPlus;
+  public faBell = faBell;
 
   public isloggedIn(): boolean {
     return this.authService.isLoggedIn();
   }
 
-  public toggleSidebar():void {
+  public toggleSidebar(): void {
     this.sidebarService.toggle();
   }
 
-    public goToCreatePostPage(): void {
+  public goToCreatePostPage(): void {
     this.router.navigate(['/submit']);
   }
 }
